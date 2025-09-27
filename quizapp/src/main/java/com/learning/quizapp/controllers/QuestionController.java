504bb9046079335b22b65db2566e59cddd613558
harishmehra772 <harishmehra772@gmail.com>
2025-09-27 21:59:10 +0530

    added exception handling and responseentity changes

--- quizapp/src/main/java/com/learning/quizapp/controllers/QuestionController.java
+++ quizapp/src/main/java/com/learning/quizapp/controllers/QuestionController.java
@@ -3,6 +3,7 @@ package com.learning.quizapp.controllers;
 import com.learning.quizapp.model.Question;
 import com.learning.quizapp.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,17 +15,29 @@ public class QuestionController {
     private QuestionService questionsService;
 
     @GetMapping("questionsList")
-    public List<Question> questions(){
+    public ResponseEntity<List<Question>> questions(){
         return this.questionsService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> questionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> questionsByCategory(@PathVariable String category){
         return this.questionsService.getQuestionsByCategory(category);
     }
 
     @PostMapping("addQuestion")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return this.questionsService.addQuestion(question);
     }
+
+    @PutMapping("updateQuestion")
+    public ResponseEntity<String> updateQuestion(@RequestBody Question question){
+        return this.questionsService.addQuestion(question);
+    }
+
+    @DeleteMapping("deleteQuestion/{id}")
+    public ResponseEntity<String> deleteQuestion(@PathVariable Integer id){
+        return this.questionsService.deleteQuestion(id);
+    }
+
+
 }
--- quizapp/src/main/java/com/learning/quizapp/services/QuestionService.java
+++ quizapp/src/main/java/com/learning/quizapp/services/QuestionService.java
@@ -3,24 +3,55 @@ package com.learning.quizapp.services;
 import com.learning.quizapp.dao.QuestionDao;
 import com.learning.quizapp.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class QuestionService {
     @Autowired
     QuestionDao questionDao;
-    public List<Question> getAllQuestions(){
-        return this.questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try{
+            return new ResponseEntity<>(this.questionDao.findAll(),HttpStatus.OK);
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return this.questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(this.questionDao.findByCategory(category),HttpStatus.OK);
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question question){
-        this.questionDao.save(question);
-        return "Success";
+    public ResponseEntity<String> addQuestion(Question question){
+        try{
+            this.questionDao.save(question);
+            return new ResponseEntity<>("Success",HttpStatus.CREATED);
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+        return new ResponseEntity<>("Error",HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> deleteQuestion(Integer id) {
+        try{
+            this.questionDao.deleteById(id);
+            return new ResponseEntity<>("Success",HttpStatus.OK);
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+        return new ResponseEntity<>("NOT FOUND",HttpStatus.NO_CONTENT);
     }
 }
